2ae8df9f25700775ff0ccb730c5d4801ef137eb0
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-18 05:08:59 -0400

    Implement country codes

---
+++ geolocate-core/src/country.rs
@@ -0,0 +1,120 @@
+use std::fmt::{Display, Write};
+use std::str::{Chars, FromStr};
+
+use serde::de::{Unexpected, Visitor};
+use serde::{Deserialize, Serialize};
+
+/// An error that is returned when trying to parse an invalid country code.
+#[repr(transparent)]
+#[derive(Clone, Debug, Hash, PartialEq, Eq)]
+pub struct InvalidCodeError(Box<str>);
+
+impl std::error::Error for InvalidCodeError {}
+
+impl Display for InvalidCodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid country code: {}", self.0)
+    }
+}
+
+/// A country's code.
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+pub enum CountryCode {
+    /// An Alpha-2 code.
+    Alpha2([char; 2]),
+    /// An Alpha-3 code.
+    Alpha3([char; 3]),
+    /// An Alpha-4 code.
+    Alpha4([char; 4]),
+    /// An unassigned code.
+    Unassigned,
+}
+
+impl From<[char; 2]> for CountryCode {
+    #[inline]
+    fn from(value: [char; 2]) -> Self {
+        Self::Alpha2(value)
+    }
+}
+
+impl From<[char; 3]> for CountryCode {
+    #[inline]
+    fn from(value: [char; 3]) -> Self {
+        Self::Alpha3(value)
+    }
+}
+
+impl From<[char; 4]> for CountryCode {
+    #[inline]
+    fn from(value: [char; 4]) -> Self {
+        Self::Alpha4(value)
+    }
+}
+
+impl FromStr for CountryCode {
+    type Err = InvalidCodeError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        #[inline]
+        fn array<const N: usize>(iter: &mut Chars<'_>) -> [char; N] {
+            std::array::from_fn(|_| iter.next().unwrap_or_else(|| unreachable!("a character is missing")))
+        }
+
+        let mut chars = value.chars();
+
+        Ok(match value.chars().count() {
+            2 => Self::Alpha2(array(&mut chars)),
+            3 => Self::Alpha3(array(&mut chars)),
+            4 => Self::Alpha4(array(&mut chars)),
+            _ => return Err(InvalidCodeError(value.into())),
+        })
+    }
+}
+
+impl Display for CountryCode {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let slice: &[char] = match self {
+            Self::Alpha2(array) => array,
+            Self::Alpha3(array) => array,
+            Self::Alpha4(array) => array,
+            Self::Unassigned => return write!(f, "??"),
+        };
+
+        slice.iter().try_for_each(|c| f.write_char(*c))
+    }
+}
+
+impl Serialize for CountryCode {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(&self.to_string())
+    }
+}
+
+impl<'de> Deserialize<'de> for CountryCode {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct CodeVisitor;
+
+        impl Visitor<'_> for CodeVisitor {
+            type Value = CountryCode;
+
+            fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                write!(f, "a valid country code")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                CountryCode::from_str(v).map_err(|_| E::invalid_value(Unexpected::Str(v), &self))
+            }
+        }
+
+        deserializer.deserialize_str(CodeVisitor)
+    }
+}
--- geolocate-core/src/lib.rs
+++ geolocate-core/src/lib.rs
@@ -1,14 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![cfg_attr(not(debug_assertions), deny(clippy::expect_used, clippy::panic, clippy::unwrap_used))]
+#![cfg_attr(not(debug_assertions), warn(missing_docs))]
+#![cfg_attr(debug_assertions, warn(clippy::expect_used, clippy::panic, clippy::unwrap_used))]
+#![warn(clippy::nursery, clippy::pedantic, clippy::todo)]
+#![allow(clippy::module_name_repetitions)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Defines countries and their API.
+pub mod country;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/// The library's default import prelude.
+pub mod prelude {}
