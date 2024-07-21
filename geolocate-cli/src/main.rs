112b5ec68bcb0fbcefb7d33fff322509886c1923
Jaxydog <32988901+Jaxydog@users.noreply.github.com>
2024-07-20 22:49:27 -0400

    Add count command

--- geolocate-cli/src/main.rs
+++ geolocate-cli/src/main.rs
@@ -44,6 +44,18 @@ pub struct Arguments {
 #[derive(Clone, Debug, Hash, PartialEq, Eq, Subcommand)]
 #[command(about, author, long_about = None)]
 pub enum Command {
+    /// Tallies the number of IP addresses assigned per country.
+    Count {
+        /// Only display the specified number of countries.
+        #[arg(short = 'l', long = "limit")]
+        limit: Option<usize>,
+        /// Only count IP addresses in the IPv4 format.
+        #[arg(short = '4', long = "v4")]
+        v4: bool,
+        /// Only count IP addresses in the IPv6 format.
+        #[arg(short = '6', long = "v6")]
+        v6: bool,
+    },
     /// Resolves a single IP address' country of origin.
     Resolve {
         /// The IP address to resolve.
@@ -93,25 +105,81 @@ pub fn main() -> Result<()> {
     let ipv6_map = crate::map::parse_ipv6_map_file(&arguments.ipv6_source, None, resolve)?;
 
     match arguments.command {
-        command @ Command::Resolve { .. } => crate::resolve(command, &ipv4_map, &ipv6_map),
+        ref command @ Command::Count { .. } => crate::count(command, &ipv4_map, &ipv6_map),
+        ref command @ Command::Resolve { .. } => crate::resolve(command, &ipv4_map, &ipv6_map),
     }
+}
+
+/// Counts the mapped countries.
+///
+/// # Errors
+///
+/// This function will return an error if the given command contains invalid arguments.
+pub fn count(
+    command: &Command,
+    ipv4: &Ipv4AddrBlockMap<MaybeCountry>,
+    ipv6: &Ipv6AddrBlockMap<MaybeCountry>,
+) -> Result<()> {
+    let Command::Count { limit, v4, v6 } = command else {
+        bail!("invalid command type");
+    };
+    if (!v4 && !v6) || (*v4 && *v6) {
+        bail!("one of `v4` or `v6` must be enabled");
+    }
+
+    if limit.is_some_and(|n| n == 0) {
+        return Ok(());
+    }
+
+    let mut countries = HashMap::<MaybeCountry, usize>::new();
+
+    if *v4 {
+        for country in ipv4.values().cloned() {
+            *countries.entry(country).or_default() += 1;
+        }
+    } else {
+        for country in ipv6.values().cloned() {
+            *countries.entry(country).or_default() += 1;
+        }
+    }
+
+    let mut sorted = countries.into_iter().collect::<Box<[_]>>();
+
+    sorted.sort_unstable_by_key(|(_, n)| *n);
+    sorted.reverse();
+
+    if let Some(limit) = *limit {
+        for (country, count) in sorted.iter().take(limit) {
+            println!("{country}: {count}");
+        }
+    } else {
+        for (country, count) in &sorted {
+            println!("{country}: {count}");
+        }
+    }
+
+    Ok(())
 }
 
 /// Resolves an IP address.
 ///
 /// # Errors
 ///
-/// This function will return an error if the functions arguments are invalid or contradicting.
+/// This function will return an error if the given command contains invalid arguments.
 pub fn resolve(
-    Command::Resolve { address, v4, v6, name, alpha_2, numeric }: Command,
+    command: &Command,
     ipv4: &Ipv4AddrBlockMap<MaybeCountry>,
     ipv6: &Ipv6AddrBlockMap<MaybeCountry>,
 ) -> Result<()> {
+    let Command::Resolve { address, v4, v6, name, alpha_2, numeric } = command else {
+        bail!("invalid command type");
+    };
+
     if !name && !alpha_2 && !numeric {
         return Ok(());
     }
 
-    let Some(country) = (match (address, v4, v6) {
+    let Some(country) = (match (*address, v4, v6) {
         (IpAddr::V4(address), true, _) | (IpAddr::V4(address), false, false) => ipv4.get_from_address(address),
         (IpAddr::V6(address), _, true) | (IpAddr::V6(address), false, false) => ipv6.get_from_address(address),
         _ => bail!("invalid command arguments"),
@@ -121,24 +189,24 @@ pub fn resolve(
 
     match country {
         MaybeCountry::Missing(code) => {
-            if name {
+            if *name {
                 eprintln!("missing country name for alpha-2 code '{code}'");
             }
-            if alpha_2 {
+            if *alpha_2 {
                 println!("{code}");
             }
-            if numeric {
+            if *numeric {
                 eprintln!("missing numeric code for alpha-2 code '{code}'");
             }
         }
         MaybeCountry::Present(country) => {
-            if name {
+            if *name {
                 println!("{}", country.name);
             }
-            if alpha_2 {
+            if *alpha_2 {
                 println!("{}", country.code);
             }
-            if numeric {
+            if *numeric {
                 println!("{}", country.numeric);
             }
         }
